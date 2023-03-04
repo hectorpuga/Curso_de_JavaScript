7ad1627cd17a8144793e93dd3f1fcea4de65d392
Hector Puga <pugagarciah@gmail.com>
2023-03-04 13:22:10 -0600

    Protip de operador ternario

---
+++ assets/js/protip-ternario.js
@@ -0,0 +1,32 @@
+
+
+const elMayor = (a, b) => (a > b) ? a : b;
+const tieneMembresia = (miembro) => (miembro) ? '2 Dólares' : '10 Dolares';
+
+console.log(elMayor(20, 15));
+console.log(tieneMembresia(false))
+
+
+
+const amigo = false;
+
+const amigosArr = [
+
+    'Peter',
+    'Tony',
+    'Dr. Strange',
+    amigo ? 'Thor' : 'Loky',
+
+    // (()=>'Nick Fury')()
+
+    elMayor(10, 15)
+];
+
+console.log(amigosArr);
+
+const nota = 82.5;  // A+ A B+
+
+
+const grado = nota >= 95 ? 'A+' : nota >= 90 ? 'A' : nota >= 85 ? 'B+' : nota >= 80 ? 'B' : nota >= 75 ? 'C+' : nota >= 70 ? 'C' : 'F';
+
+console.log({ nota, grado });
