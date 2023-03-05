ce6bd83cc938179e2f749395815626a660c8f68e
Hector Puga <pugagarciah@gmail.com>
2023-03-05 01:14:49 -0600

    Ciclos while y do-while

---
+++ assets/js/ciclos/while-do-while.js
@@ -0,0 +1,36 @@
+
+
+const carros=['Ford','Mazda','Honda','Toyota'];
+
+let i=0;
+
+// while(i<carros.length){
+
+//     console.log(carros[i]);
+//     i++;
+
+// }
+
+console.warn("While");
+
+while(carros[i]){
+
+    if(i===1){
+        i++;
+
+        continue;
+    }
+
+    console.log(carros[i]);
+    i++;
+
+}
+
+
+console.warn("Do While");
+let j=10;
+
+do{
+console.log(carros[j])
+j++;
+}while(carros[j]);
